d383206d394f29466dcddb2b0f4d935ec2fcc975
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2021-11-11 13:43:48 +0200

    решает 8ю задачу

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector('.login-form');
+const label = formEl.querySelector('label');
 
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    // const email = event.currentTarget.elements.email.value;
+    // const password = event.currentTarget.elements.password.value;
+    
+    const elements = event.currentTarget.elements;
+    
+    const email = elements.email.value;
+    const password = elements.password.value;
+    
+    if (email === '' || password === '') {
+        window.alert('Милый друг, заполни все поля, пожалуйста :)');
+}
+    
+    const formData = {
+        email,
+        password
+    }
+    console.log(formData);
+    
+}
