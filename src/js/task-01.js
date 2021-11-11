0cefa4cf7f454ac9fdb8082eb9802b6a4fe6e9cb
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2021-11-11 17:31:05 +0200

    вносит правки в 1 задачу

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,27 +1,9 @@
 const listItemEl = document.querySelectorAll('.item');
+
 console.log(`Number of categories: ${listItemEl.length}`);
 
+listItemEl.forEach(el => {
+    console.log(`Category: ${el.querySelector('h2').textContent}`);
+    console.log(`Elements: ${el.querySelectorAll('.item li').length}`)
 
-const animalsEl = listItemEl[0].querySelector('h2');
-console.log(`Category: ${animalsEl.textContent}`);
-console.log(`Elements: ${listItemEl[0].querySelectorAll('li').length}`)
-
-const productsEl = listItemEl[1].querySelector('h2');
-console.log(`Category: ${productsEl.textContent}`);
-console.log(`Elements: ${listItemEl[1].querySelectorAll('li').length}`)
-
-const technologiesEl = listItemEl[2].querySelector('h2');
-console.log(`Category: ${technologiesEl.textContent}`);
-console.log(`Elements: ${listItemEl[2].querySelectorAll('li').length}`)
-
-
-// const animalsSumDef = listItemDef[0].querySelectorAll('li');
-// console.log(`Elements: ${animalsDef.length}`)
-// console.log(`Elements: ${animalsSumDef.length}`);
-
-// const items = document.querySelectorAll('.item');
-// console.log(items);
-// for (let item of items) {
-    
-// }
-
+})
